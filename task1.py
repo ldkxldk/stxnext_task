10af53fcb24f7602e2e4fcd16e9681e850ef9556
ldkxldk <ldk.x.ldk@gmail.com>
2013-05-21 13:34:25 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -15,16 +15,16 @@ jinja2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), au
 
 
 class Handler(webapp2.RequestHandler):
-
-  def write(self,*a, **kw):
+	
+	def write(self,*a, **kw):
 		self.response.out.write(*a, **kw)
-
+	
 	def render_str(self, template, **params):
 		t = jinja2_env.get_template(template)
 		return t.render(params)
 
-	def render(self, template, **kw):
-		self.write(self.render_str(template, **kw))
+  	def render(self, template, **kw):
+  		self.write(self.render_str(template, **kw))
 		
 class Task(ndb.Model):
 	

be5185fa6894d39ec56abecf6dac3f0731a8886a
ldkxldk <ldk.x.ldk@gmail.com>
2013-05-22 00:37:26 +0300

    8h...
    To co zrobiłem:
    - przepisałem klasę Main, dodałem nowe funkcje, skorzystałem z projection to uzyskania wybranych wyników
    - obecnie tworzone są nowe obiekty w bazie (a nie jak to było wcześniej nadpisywane)
    - poprawiłem klasę Result
    Zrobiłbym to w jakieś 3-4h krócej gdyby nie to, że nie wiedziałem jak sobie przesłać id do funckji get() w klasie Result. Na szczęście wymyśliłem użycie global, nim wyrzuciłem komputer przez okno. Już nie pierwszy raz global uratowało mi sprzęt z tego co pamiętam. Bardzo chętnie dowiem się jak uzyskać konkretny element z bazy danych bez użycia global w tej konkretnej sytuacji.
    Muszę tu jeszcze zrobić:
    - klasę List, która obsłuży mi działanie cronowe i wyrysuje tabelkę z bazy danych
    - stworzyć asynchroniczne urlfetch zamiast urllib2
    - chciałem też użyć query cursor, żeby nie wyświetlać wszystkich wyników na raz
    - zastanowię się nad użyciem query iterators z keys_only lub projection (któreś jest bardziej efektywne? bo na chwilę obecną sprowadzają się dla mnie do tego samego)
    Postaram się dokończyć zadanie jutro o ile znowu nie zawędruję w ciemną szczelinę programistycznego odbytu...

---
+++ task2.py
@@ -0,0 +1,117 @@
+#-*- coding: utf-8 -*-
+import os
+import webapp2
+import jinja2
+import re
+import string
+import logging
+import urllib2
+
+from google.appengine.api import taskqueue
+from google.appengine.ext import ndb
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+
+class Handler(webapp2.RequestHandler):
+
+  def write(self,*a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja2_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+		
+class Task(ndb.Model):
+
+	word = ndb.StringProperty(required=True)
+	count = ndb.IntegerProperty()
+
+class MainPage(Handler):
+
+	def get(self):
+		self.render('main.html')
+	
+	def post(self):
+		word = self.request.get('word')
+		self.decide(word)
+		
+	def projection(self, word):
+		ids = Task.query().fetch(projection=['word'])
+		return ids
+	
+	def process_fetch(self,word):
+		ids = self.projection(word)
+		for i in ids:
+			#logging.error('%s'%i.word)
+			if i.word == word:
+				return False
+
+	def decide(self,word):
+		if not self.process_fetch(word):
+			task = Task(id=word, word=word)
+			task.put()
+		global id
+		id = word
+		taskqueue.add(url='/result',params={'id': word})
+		self.redirect('/result')
+
+
+
+class Result(Handler):
+
+	def get(self):
+		search = Task.get_by_id(id)
+		if search.count == None:
+			self.response.out.write('ZADANIE W TOKU, PROSZĘ ODSWIEZYĆ')
+		else:
+			self.response.out.write('SLOWO WYSTEPUJE {0} RAZY.'.format(search.count))
+
+	def post(self):
+		tag = self.request.get('id')
+		search = Task.get_by_id(tag)
+		SUM = self.count_word(search)
+		search.count = SUM
+		search.put()
+
+
+	def count_word(self,search):
+		url = ['http://onet.pl/','http://interia.pl/','http://wp.pl/','http://redtube.com/','http://www.gazeta.pl/0,0.html',
+		'http://www.pudelek.pl/','http://www.kozaczek.pl/','http://www.plotek.pl/plotek/0,0.html','http://www.thetimes.co.uk/tto/news/','http://edition.cnn.com/']
+		shots = 0
+		for u in url:
+			result = urllib2.urlopen(u)
+			text = self.prepare_text(result)
+			shots += self.count(text,search.word)
+		return shots
+	
+	def count(self,text,word):
+		shots = 0
+		for line in text:
+			shots += len(re.findall('(?<![a-zA-Z0-9]){0}'.format(word),line))
+		return shots
+
+	def prepare_text(self,result):
+		pure_text = []
+		for line in result:
+			check=True
+			while check:
+				look = re.search('<.*?>',line)
+				if look:
+					line = line[:look.span()[0]]+line[look.span()[1]:]
+				else:
+					check=False
+					pure_text.append(line)
+		return pure_text
+		
+class List(Handler):
+	
+	def get(self):
+		return
+
+
+app = webapp2.WSGIApplication([('/search',MainPage),('/result',Result),('/list',List)], debug=True)
